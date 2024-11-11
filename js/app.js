e8fd658d07d237ef08ec927526e7b86ca694d88b
Wizdomami <138355859+EynajaCodes@users.noreply.github.com>
2024-11-11 15:04:33 -0500

    Working on some functions for adding nav items to the nav bar at the top of the page

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@
  * 
 */
 
+
+
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
@@ -23,14 +25,23 @@
  * 
 */
 
+const navBarList = document.querySelector('#navbar__list');
+const navBarMenu = document.querySelector('.navbar__menu');
+const sectionAll = document.querySelectorAll('section');
+const classesArray = [section, your-active-class, navBarMenu];
+const activeClass = document.querySelector(.your-active-class);
+
+
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+window.activeClass(0,0);
+const makeCurrentSection = ()=> {
 
-
+}
 
 /**
  * End Helper Functions
@@ -53,10 +64,42 @@
  * 
 */
 
+// creates function that adds li to navigation bar UL using Js 
+
+
+const addToNav = () => { 
+    for (section of sectionAll) { 
+       const newListItem = document.createElement('li');
+       newListItem.innerHTML= `<a href="#${section.id}" class = "menu__link"> ${section.dataset.nav}</a>`; 
+       navBarList.appendChild(newListItem);
+    }
+    navBarMenu.appendChild(navBarList);
+}
+
+
 // Build menu 
 
+addToNav();
+
 // Scroll to section on link click
+const sectionScroll = document.querySelectorAll('menu__link') 
+
+
+let moveToSection =() => {
+
+for (section in sectionAll) { 
+    const currentSection = window.scrollTo('section');
+    
+}
+
+sectionScroll.addEventListener('click', moveToSection);
+
+
+
+
+};
 
 // Set sections as active
 
 
+sectionAll = Active
